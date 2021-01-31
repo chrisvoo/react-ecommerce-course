c76b81cd465375c36fbf21205d3a6e3a5e9d4e80
Christian Castelli <voodoo81people@gmail.com>
2021-01-31 12:25:41 +0100

    Section 4 complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Homepage } from './pages/homepage/Homepage';
+import './App.scss';
 
 function App() {
   return (
--- src/components/menu-item/MenuItem.tsx
+++ src/components/menu-item/MenuItem.tsx
@@ -3,7 +3,7 @@ import './MenuItem.scss';
 
 export const MenuItem = ({ title, imageUrl, size }: DirectorySection) => {
     return (
-        <div style={{ backgroundImage: `url('${imageUrl}')` }} className={`menu-item ${size}`}>
+        <div style={{ backgroundImage: `url('${imageUrl}')` }} className={`menu-item ${size ?? ''}`}>
             <div className="content">
                 <h1 className="title">{title}</h1>
                 <span className="subtitle">SHOP NOW</span>
